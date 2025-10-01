1c878fb898c4c8d51eb3a84411f45bae6d37d395
Barteneva156 <Fairyfromwinx@mail.ru>
2025-10-01 13:12:36 +0300

    gjhf

---
+++ add.php
@@ -0,0 +1,19 @@
+<?php var_dump($_POST);
+require_once('config.php');
+    if ($_POST) {
+    $name = trim($_POST['name']);
+    $message = trim($_POST['message']);
+
+    if ($name && $message) {
+        
+        $stmt = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?, ?)");
+        $stmt->execute([$name, $message]);
+
+        
+        header('Location: index.php');
+        exit;
+    } else {
+        $error = "Пожалуйста, заполните все поля.";
+    }
+    }
+?>
--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<?php 
-require_once 'config.php';
-//var_dump($_POST);
-if ($_POST) {
-    $name = trim($_POST['name']);
-    $message = trim($_POST['message']);
-
-    if ($name && $message) {
-        $stmt = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?, ?)");
-        $stmt->execute([$name, $message]);
-
-        header('Location: index.php');
-        exit;
-    } else {
-        $error = "Пожалуйста, заполните все поля.";
-    }
-}
+<?php
+    require_once('config.php');
 $stmt = $pdo->query("SELECT * FROM messages ORDER BY created_at DESC");
 $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -25,46 +10,33 @@ $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-        body { font-family: Arial; margin: 40px; background: #f0f0f0; }
-        .container { max-width: 600px; margin: auto; background: white; padding: 20px; border-radius: 8px; }
-        .message { padding: 15px; border-bottom: 1px solid #eee; }
-        .name { font-weight: bold; color: #333; }
-        .date { font-size: 0.8em; color: #888; }
-        input, textarea { width: 100%; padding: 8px; margin: 5px 0 15px; }
-        button { padding: 10px 20px; background: #007BFF; color: white; border: none; cursor: pointer; }
-        button:hover { background: #0056b3; }
-        .error { color: red; }
-    </style>
-
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <form method="POST">
-            <input type="text" name="name" placeholder="Ваше имя" required>
-            <textarea name="message" rows="4" placeholder="Ваше сообщение" required></textarea>
-            <button type="submit">Отправить</button>
-        </from>
+<div class="box">
+    <form method="POST" action = "add.php">
+        <input type="text" name="name" placeholder="Your name" required>
+        <textarea name="message" rows="4" placeholder="Your message" required></textarea>
+        <button type="submit">Отправить</button>
+    </form>
 
-        <h3>Сообщения (<?= count($messages) ?>):</h3>
-        <?php if (empty($messages)): ?>
-            <p>Пока нет сообщений. Будьте первым!</p>
-        <?php else: ?>
-            <?php foreach($messages as $msg): ?>
-                <div class="message">
-                    <div class="name"> <?= htmlspecialchars($msg['name']) ?></div>
-                    <div><?= htmlspecialchars($msg['message']) ?></div>
-                    <div class="date"> <?= $msg['created_at'] ?></div>
+    <h3>Сообщение <?= count($messages) ?></h3>
+
+    <?php if (empty($messages)): ?>
+        <p>Будьте первым!</p>
+    <?php else:?>
+        <?php foreach ($messages as $msg): ?>
+            <div class="message">
+                <div class="name">
+                    <?= htmlspecialchars($msg['name']) ?>
                 </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
+                <div class="date">
+                    <?= $msg['created_at'] ?>
+                </div>
+            </div>
+            <hr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
 </body>
-    
-
 </html>
